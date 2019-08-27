da58b07d72f6091756ee1433e6cde00989822fef
Ярослав Горожанкин <yarik1706@mail.ru>
2019-08-27 12:57:59 +0300

    Update VSphereApplication.java

--- src/main/java/com/vsphere/VSphereApplication.java
+++ src/main/java/com/vsphere/VSphereApplication.java
@@ -13,10 +13,6 @@ import java.util.Arrays;
 /**
  * Start application from command line:
  * java -jar <path>\vSphere-0.1.0.jar --url=<url> --username=<username> --password=<password>
- * Default variables:
- * url=10.240.3.120
- * username=Student@tn.local
- * password=12qwAZSX#E
  */
 @SpringBootApplication
 public class VSphereApplication implements ApplicationRunner {
